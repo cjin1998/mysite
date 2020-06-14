f6738c82f4c4562c9d6a1fbc8dd5f42f0f5071f9
cjin1998 <christinejin9@gmail.com>
2020-06-13 23:27:44 -0400

    added nav bar and routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/info", function(req, res, next) {
+  res.render("generic", { title: "Generic" });
+});
+
+router.get("/elements", function(req, res, next) {
+  res.render("elements", { title: "Elements" });
 });
 
 module.exports = router;
