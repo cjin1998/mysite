6591324f7dbba320e24ae4c7d29e527443be6fa9
cjin1998 <christinejin9@gmail.com>
2020-06-18 03:05:42 -0400

    finally figured out grid stuff

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get("/", function(req, res, next) {
 });
 
 router.get("/info", function(req, res, next) {
-  res.render("ball", { title: "Generic" });
+  res.render("generic", { title: "Generic" });
 });
 
 router.get("/elements", function(req, res, next) {
